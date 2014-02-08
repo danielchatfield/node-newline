d0e941a9af3abbf6be3367383ea7f686e07879ca
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-02-08 17:12:43 +0000

    Moved detect test script

---
+++ test/detect.js
@@ -0,0 +1,29 @@
+"use strict";
+
+require("should");
+var detect = require("../lib/detect.js");
+
+var generateSample = function(lineEnd) {
+  return "This is a test" + lineEnd +
+         "file with some " + lineEnd +
+         "new lines, useful" + lineEnd +
+         "testing the library";
+};
+
+var lf = generateSample("\n");
+var cr = generateSample("\r");
+var crlf = generateSample("\r\n");
+
+describe('lineEndings.detect', function () {
+  it('should detect LF', function() {
+    detect(lf).should.equal("LF");
+  });
+
+  it('should detect CR', function() {
+    detect(cr).should.equal("CR");
+  });
+
+  it('should detect CRLF', function() {
+    detect(crlf).should.equal("CRLF");
+  });
+});
