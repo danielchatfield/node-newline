8196b7c50051ebd67dfd4f4dc15eaa5a2f9c2200
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-02-08 17:13:02 +0000

    Added mocha testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,32 @@
 "use strict";
 
 var gulp = require('gulp');
+var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var mocha = require('gulp-mocha');
 
 var paths = {
-  scripts: ['gulpfile.js', 'index.js', 'lib/*', 'test/*.js']
+  scripts: ['gulpfile.js', 'index.js', 'lib/*', 'test/*']
 };
 
 gulp.task('lint', function(){
   var jshintConfig = require("./package").jshintConfig;
 
   gulp.src(paths.scripts)
+    .pipe(jscs())
     .pipe(jshint(jshintConfig))
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['lint']);
+gulp.task('unittests', function(){
+  gulp.src(paths.scripts)
+    .pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['lint', 'test']);
+});
+
+gulp.task('test', ['lint', 'unittests']);
+gulp.task('default', ['test']);
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 "use strict";
+/*jshint expr: true*/
 
-var should = require("should");
+require("should");
 var lineEndings = require("../index.js");
 
 describe('lineEndings api', function () {
   it('should have a detect method', function() {
     lineEndings.detect.should.be.a.Function;
-  }
+  });
 });
